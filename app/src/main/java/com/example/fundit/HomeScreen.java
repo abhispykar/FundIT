dba840597a7303c286b4aa715a86c6bf4e3c7e6a
Abhi <abhispykar1812@gmail.com>
2023-10-26 23:24:49 +0530

    Login continued

--- app/src/main/java/com/example/fundit/HomeScreen.java
+++ app/src/main/java/com/example/fundit/HomeScreen.java
@@ -2,6 +2,7 @@ package com.example.fundit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class HomeScreen extends AppCompatActivity {
 
     EditText username,password;
     Button signup,signin;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +23,21 @@ public class HomeScreen extends AppCompatActivity {
         password = (EditText) findViewById(R.id.et_password);
         signup = (Button) findViewById(R.id.btn_signup);
         signin = (Button) findViewById(R.id.btn_signin);
+        DB = new DBHelper(this);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
 
             }
         });
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
 
             }
         });
--- app/src/main/java/com/example/fundit/LoginActivity.java
+++ app/src/main/java/com/example/fundit/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.fundit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText username,password;
+    Button btnlogin;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username=(EditText) findViewById(R.id.username);
+        password=(EditText) findViewById(R.id.password);
+        btnlogin=(Button) findViewById(R.id.btnsignin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }

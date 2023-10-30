1824c6d4da88261c71be3816ef39d3bfad144fb1
Abhi <abhispykar1812@gmail.com>
2023-10-30 15:02:04 +0530

    added sample images in caraousel and worked on UI

--- app/src/main/java/com/example/fundit/Dashboard.java
+++ app/src/main/java/com/example/fundit/Dashboard.java
@@ -5,22 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.DropBoxManager;
 
+import com.example.fundit.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
+
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Dashboard extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
 
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationView = findViewById(R.id.navigationview);
@@ -32,7 +40,68 @@ public class Dashboard extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.naviopen, R.string.naviclose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        initSlider();
 
 
     }
+
+    private void initSlider() {
+        // Java
+        ImageCarousel carousel = findViewById(R.id.carousel);
+
+// Register lifecycle. For activity this will be lifecycle/getLifecycle() and for fragments it will be viewLifecycleOwner/getViewLifecycleOwner().
+        carousel.registerLifecycle(getLifecycle());
+
+        List<CarouselItem> list = new ArrayList<>();
+
+// Image URL with caption
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080",
+                        "Photo by Aaron Wu on Unsplash"
+                )
+        );
+        // Just image URL
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080"
+                )
+        );
+
+// Image URL with header
+        Map<String, String> headers = new HashMap<>();
+        headers.put("header_key", "header_value");
+
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080",
+                        headers
+                )
+        );
+
+//// Image drawable with caption
+//        list.add(
+//                new CarouselItem(
+//                        R.drawable.fundit_logo_2,
+//                        "Photo by Kimiya Oveisi on Unsplash"
+//                )
+//        );
+//
+//// Just image drawable
+//        list.add(
+//                new CarouselItem(
+//                        R.drawable.fundit_logo
+//                )
+//        );
+//
+//// ...
+
+        carousel.setData(list);
+    }
+
+
+
+
+
+
 }

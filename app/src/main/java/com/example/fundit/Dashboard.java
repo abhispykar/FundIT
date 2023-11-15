379beb97292cde171cfc0b19a021e910d773fb71
tusharsoni142001 <tusharsoni142001@gmail.com>
2023-11-15 19:06:26 +0530

    Added intent on menu options

--- app/src/main/java/com/example/fundit/Dashboard.java
+++ app/src/main/java/com/example/fundit/Dashboard.java
@@ -1,8 +1,10 @@
 package com.example.fundit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
@@ -10,9 +12,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.DropBoxManager;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewConfiguration;
+import android.widget.Toast;
 
 import com.example.fundit.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
@@ -26,7 +30,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
@@ -46,30 +50,32 @@ public class Dashboard extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(Dashboard.this, drawerLayout, toolbar, R.string.naviopen, R.string.naviclose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
         initSlider();
         getOverflowMenu();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.sidemenu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        int id=item.getItemId();
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.sidemenu, menu);
+//        return true;
+//    }
 
-        if(id==R.id.account)
-        {
-            Intent intent=new Intent(Dashboard.this,StartupFounderProfile.class);
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item)
+//    {
+//        int id=item.getItemId();
+//
+//        if(id==R.id.account)
+//        {
+//            Intent intent=new Intent(Dashboard.this,StartupFounderProfile.class);
+//            startActivity(intent);
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void getOverflowMenu() {
         try {
@@ -139,4 +145,22 @@ public class Dashboard extends AppCompatActivity {
         carousel.setData(list);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.account:
+                Intent intent =new Intent(Dashboard.this,StartupFounderProfile.class);
+                startActivity(intent);
+                break;
+            case R.id.aboutus:
+                Toast.makeText(this,"About Us",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(this,"Logout",Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
